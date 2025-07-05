2a1703f7d3ae0c802a4e5fc34cd36a5d940186f0
Marco Crotta <marco.crotta@conio.com>
2025-07-05 17:15:09 +0200

    small fix

--- contracts/trigodeck.sol
+++ contracts/trigodeck.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
-import {// Subcall} from "@oasisprotocol/sapphire-contracts/contracts/// Subcall.sol";
+import {Subcall} from "@oasisprotocol/sapphire-contracts/contracts/Subcall.sol";
 import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -82,7 +82,7 @@ contract Trigo_Deck /* is Ownable */ {
         }
     }
 
- 
+
     /**************************************************************************
      * USER FUNCTIONS
      */
